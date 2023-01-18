10aec5c0a67bce65e7c04ec03a7a669b77f699ee
Marouane Skandaji <marouane.skandaji@gmail.com>
2023-01-18 21:08:38 +0100

    [fix;test]: unit tests for helper functions - TODO: add more test cases

---
+++ tests/test_unpack.py
@@ -0,0 +1,29 @@
+"""
+Unit tests for imdb_rating_classifier/util/unpack.py
+
+@TODO: add unit tests
+"""
+from __future__ import annotations
+
+import typing as t
+from unittest.mock import patch
+
+import pytest  # noqa: F401 # imported but unused
+
+from imdb_rating_classifier.util.unpack import unpack_contents
+
+
+def test_unpack_contents(capsys: t.Any) -> None:
+    """
+    Test the unpack_contents function, the request object is mocked.
+    If the response returns a 200 status code, the content should be printed to the console.
+    Else, it will raise an exception.
+
+    :param capsys: pytest fixture
+    """
+    with patch('imdb_rating_classifier.util.unpack.requests') as mock_requests:
+        mock_requests.get.return_value.status_code = 200
+        mock_requests.get.return_value.text = 'test'
+        unpack_contents(mock_requests.get.return_value)
+        captured = capsys.readouterr()
+        assert captured.out == ''
