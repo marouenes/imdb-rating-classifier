4c634a7ae6fba226eb03d5d2a9d510d98f12b95e
Marouane Skandaji <marouane.skandaji@gmail.com>
2023-01-21 14:44:00 +0100

    [fix;tests]: bigfixes for the re-penalization logic
    Changes:
    - update the unit tests and add more corner cases for None
    penalized_reviews
    - Add a conditional logic for no awards and no penalization

--- tests/test_penalizer.py
+++ tests/test_penalizer.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 
 import pytest  # noqa: F401 # imported but unused
 
-from imdb_rating_classifier.penalizer import penalize_reviews
+from imdb_rating_classifier.penalizer import oscar_calculation, penalize_reviews
 
 
 def test_penalize_reviews() -> None:
@@ -17,31 +17,31 @@ def test_penalize_reviews() -> None:
         {
             'rank': 1,
             'title': 'Foo',
-            'year': 2020,
             'votes': 2000000,
             'rating': 8.0,
+            'oscars_won': 0,
         },
         {
             'rank': 2,
             'title': 'Bar',
-            'year': 2020,
             'votes': 1000000,
             'rating': 7.0,
+            'oscars_won': 0,
         },
         {
             'rank': 3,
             'title': 'Baz',
-            'year': 2020,
             'votes': 1500000,
             'rating': 6.0,
+            'oscars_won': 0,
         },
         # edge case where the max number of votes is the same one other movie
         {
             'rank': 4,
             'title': 'Qux',
-            'year': 2020,
             'votes': 2000000,
             'rating': 5.0,
+            'oscars_won': 0,
         },
     ]
 
@@ -56,37 +56,34 @@ def test_penalize_reviews() -> None:
     'movies, expected',
     [(
         [
-            {'rank': 1, 'title': 'Foo', 'year': 2020, 'votes': 2000000, 'rating': 8.0},
-            {'rank': 2, 'title': 'Bar', 'year': 2020, 'votes': 1000000, 'rating': 7.0},
-            # edge case where the max number of votes is the same one other movie
-            {'rank': 3, 'title': 'Qux', 'year': 2020, 'votes': 2000000, 'rating': 5.0},
+            {'rank': 1, 'votes': 2000000, 'rating': 8.0, 'oscars_won': 0},
+            {'rank': 2, 'votes': 1000000, 'rating': 7.0, 'oscars_won': 0},
+            # edge case where the max number of votes is the same for one other movie
+            {'rank': 3, 'votes': 2000000, 'rating': 5.0, 'oscars_won': 0},
         ],
         [
             {
                 'rank': 1,
-                'title': 'Foo',
-                'year': 2020,
                 'votes': 2000000,
                 'rating': 8.0,
                 'penalized': False,
+                'oscars_won': 0,
                 'penalized_rating': None,
             },
             {
                 'rank': 2,
-                'title': 'Bar',
-                'year': 2020,
                 'votes': 1000000,
                 'rating': 7.0,
                 'penalized': True,
+                'oscars_won': 0,
                 'penalized_rating': 6.0,
             },
             {
                 'rank': 3,
-                'title': 'Qux',
-                'year': 2020,
                 'votes': 2000000,
                 'rating': 5.0,
                 'penalized': False,
+                'oscars_won': 0,
                 'penalized_rating': None,
             },
         ],
@@ -104,26 +101,86 @@ def test_penalized_movie_reviews(movies: list, expected: list) -> None:
     'movies, expected',
     [(
         [
-            {'rank': 1, 'title': 'Foo', 'year': 2020, 'votes': 2000000, 'rating': 8.0},
-            {'rank': 2, 'title': 'Bar', 'year': 2020, 'votes': 2000000, 'rating': 7.0},
+            {'rank': 1, 'votes': 2000000, 'rating': 8.0, 'oscars_won': 10},
+            {'rank': 2, 'votes': 1000000, 'rating': 7.0, 'oscars_won': 1},
+            {'rank': 3, 'votes': 1500000, 'rating': 6.0, 'oscars_won': 3},
+            {'rank': 4, 'votes': 500000, 'rating': 5.0, 'oscars_won': 6},
+            {'rank': 5, 'votes': 1000000, 'rating': 4.0, 'oscars_won': 11},
         ],
         [
             {
                 'rank': 1,
-                'title': 'Foo',
-                'year': 2020,
                 'votes': 2000000,
                 'rating': 8.0,
                 'penalized': False,
+                'oscars_won': 10,
                 'penalized_rating': None,
             },
             {
                 'rank': 2,
-                'title': 'Bar',
-                'year': 2020,
+                'votes': 1000000,
+                'rating': 7.0,
+                'penalized': True,
+                'oscars_won': 1,
+                'penalized_rating': 6.3,
+            },
+            {
+                'rank': 3,
+                'votes': 1500000,
+                'rating': 6.0,
+                'penalized': True,
+                'oscars_won': 3,
+                'penalized_rating': 6.0,
+            },
+            {
+                'rank': 4,
+                'votes': 500000,
+                'rating': 5.0,
+                'penalized': True,
+                'oscars_won': 6,
+                'penalized_rating': 4.5,
+            },
+            {
+                'rank': 5,
+                'votes': 1000000,
+                'rating': 4.0,
+                'penalized': True,
+                'oscars_won': 11,
+                'penalized_rating': 4.5,
+            },
+        ],
+    )],
+)
+def test_penalized_reviews_with_oscars_awards(movies: list, expected: list) -> None:
+    """
+    Test the penalize_reviews function with a list of movies and expected results.
+    """
+    penalized_movies = penalize_reviews(movies)
+    assert penalized_movies == expected
+
+
+@pytest.mark.parametrize(
+    'movies, expected',
+    [(
+        [
+            {'rank': 1, 'votes': 2000000, 'rating': 8.0, 'oscars_won': 0},
+            {'rank': 2, 'votes': 2000000, 'rating': 7.0, 'oscars_won': 0},
+        ],
+        [
+            {
+                'rank': 1,
+                'votes': 2000000,
+                'rating': 8.0,
+                'penalized': False,
+                'oscars_won': 0,
+                'penalized_rating': None,
+            },
+            {
+                'rank': 2,
                 'votes': 2000000,
                 'rating': 7.0,
                 'penalized': False,
+                'oscars_won': 0,
                 'penalized_rating': None,
             },
         ],
@@ -134,10 +191,6 @@ def test_non_penalized_reviews(movies: list, expected: list) -> None:
     Test the penalize_reviews function, where there are no penalized reviews.
     """
     penalized_movies = penalize_reviews(movies)
-
-    for movie in penalized_movies:
-        assert movie['penalized'] is False
-
     assert penalized_movies == expected
 
 
@@ -156,3 +209,17 @@ def test_penalized_reviews_with_invalid_data() -> None:
 
     with pytest.raises(ValueError):
         penalize_reviews(movies)
+
+
+def test_no_oscar_calculation() -> None:
+    """
+    Test the oscar_calculation function with a list of movies and expected results.
+    """
+    ratings = {
+        'rank': 1,
+        'votes': 1000000,
+        'rating': 8.0,
+        'oscars_won': 10,
+        'penalized': False,
+    }
+    assert oscar_calculation(ratings) is None
